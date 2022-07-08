cf85663c64b9f2ec0dc66d175fc1db94149e364d
Andref007 <andrefps007@hotmail.com>
2022-07-07 21:01:32 -0300

    usando o método map()

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 const response = fetch("https://jsonplaceholder.typicode.com/users/")
 // Promise que trata o sucesso é o método then()
 // o parâmetro res recebe o retorno da função anterior
-.then(res => {
-    console.log(res.json())
+.then(response => {
+    console.log(response)
+    return response.json()
+})
+
+//Usamos para interar (percorrer o array),  o método map() que é um método para percorrer todo o array como um for
+.then(data => {
+    data.map(user => {
+        console.log(`Nome: ${user.name}`)
+    })
 })
 //promise que trata o erro é o método catch()
 .catch(error => {
     console.log(`Deu erro na busca: ${error}`)
 })
+
+
+//método map() só funciona com listas
+
+
+
+// site utilizado jsonplaceholder
